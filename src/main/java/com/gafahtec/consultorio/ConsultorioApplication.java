4b494ba6aee253fbcb932ddc11352fd05525e173
Giovanni Ascarza Hinostroza <gascarzah@gmail.com>
2023-04-14 16:03:41 -0500

    update

--- src/main/java/com/gafahtec/consultorio/ConsultorioApplication.java
+++ src/main/java/com/gafahtec/consultorio/ConsultorioApplication.java
@@ -39,6 +39,40 @@ public class ConsultorioApplication {
         return new AuditorAwareImpl();
     }
 
-   
+    @Bean
+    CommandLineRunner run(IRolService iRolService, IEmpresaService iEmpresaService, UsuarioService iUsuarioService, ITipoEmpleadoService iTipoEmpleadoService) {
+
+        return args -> {
+            List<Rol> roles = new ArrayList<>();
+            var rol1 = Rol.builder().idRol(1).nombre("OPE").build();
+            var rol2 = Rol.builder().idRol(2).nombre("MED").build();
+            roles.add(rol1);
+            roles.add(rol2);
+            roles.forEach(rol -> {
+                iRolService.registrar(rol);
+            });
+
+            List<Empresa> empresas = new ArrayList<>();
+            var empresa1 = Empresa.builder().idEmpresa(1).nombre("xx").build();
+            var empresa2 = Empresa.builder().idEmpresa(2).nombre("xx").build();
+            empresas.add(empresa1);
+            empresas.add(empresa2);
+            empresas.forEach(empresa -> {
+                iEmpresaService.registrar(empresa);
+            });
+            
+            List<TipoEmpleado> tipoEmpleados = new ArrayList<>();
+            var e1 = TipoEmpleado.builder().idTipoEmpleado(1).descripcion("xx").build();
+            var e2 = TipoEmpleado.builder().idTipoEmpleado(2).descripcion("Medico").build();
+            tipoEmpleados.add(e1);
+            tipoEmpleados.add(e2);
+            tipoEmpleados.forEach(tipoEmpleado -> {
+                iTipoEmpleadoService.registrar(tipoEmpleado);
+            });
+
+
+        };
+    }
+
 
 }
