3ea33ecb26a833e3840bd2931ffb6d2a10b901b0
Giovanni Ascarza Hinostroza <gascarzah@gmail.com>
2023-04-18 16:31:05 -0500

    update

--- src/main/java/com/gafahtec/consultorio/ConsultorioApplication.java
+++ src/main/java/com/gafahtec/consultorio/ConsultorioApplication.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import com.gafahtec.consultorio.dto.request.UsuarioRequest;
 import com.gafahtec.consultorio.model.Empresa;
 import com.gafahtec.consultorio.model.Rol;
 import com.gafahtec.consultorio.model.TipoEmpleado;
@@ -44,33 +45,35 @@ public class ConsultorioApplication {
 
         return args -> {
             List<Rol> roles = new ArrayList<>();
-            var rol1 = Rol.builder().idRol(1).nombre("OPE").build();
-            var rol2 = Rol.builder().idRol(2).nombre("MED").build();
+            var rol1 = Rol.builder().idRol(1).nombre("ADMIN").build();
+           
             roles.add(rol1);
-            roles.add(rol2);
+            
             roles.forEach(rol -> {
                 iRolService.registrar(rol);
             });
 
             List<Empresa> empresas = new ArrayList<>();
-            var empresa1 = Empresa.builder().idEmpresa(1).nombre("xx").build();
-            var empresa2 = Empresa.builder().idEmpresa(2).nombre("xx").build();
+            var empresa1 = Empresa.builder().idEmpresa(1).nombre("gafah").build();
+            
             empresas.add(empresa1);
-            empresas.add(empresa2);
+            
             empresas.forEach(empresa -> {
                 iEmpresaService.registrar(empresa);
             });
             
             List<TipoEmpleado> tipoEmpleados = new ArrayList<>();
-            var e1 = TipoEmpleado.builder().idTipoEmpleado(1).descripcion("xx").build();
-            var e2 = TipoEmpleado.builder().idTipoEmpleado(2).descripcion("Medico").build();
+            var e1 = TipoEmpleado.builder().idTipoEmpleado(1).descripcion("admin").build();
+            
             tipoEmpleados.add(e1);
-            tipoEmpleados.add(e2);
+          
             tipoEmpleados.forEach(tipoEmpleado -> {
                 iTipoEmpleadoService.registrar(tipoEmpleado);
             });
 
 
+            
+            
         };
     }
 
